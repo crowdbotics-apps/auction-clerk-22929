59ea84b12534256015f37ca38d36f34f8fd8df4e
Crowdbotics <team@crowdbotics.com>
2020-11-23 18:50:40 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/auction_clerk_22929/MainActivity.java
@@ -0,0 +1,15 @@
+package com.auction_clerk_22929;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "auction_clerk_22929";
+    }
+}
